e9113b06d46a60500a90d0cb69caacc204f754b6
Ayaka Miyazaki <kuromi.1031blc@gmail.com>
2025-07-14 15:17:10 -0700

    hover effect needed

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const firstName = document.getElementById('firstName')
 const lastName = document.getElementById('lastName')
 const email = document.getElementById('email')
 const message = document.getElementById('message')
-const queryType = document.querySelector('input[name="query_type"]:checked')
+// const queryType = document.querySelector('input[name="query_type"]:checked')
 const checkbox = document.getElementById('consent')
 
 form.addEventListener('submit', event =>{
@@ -15,35 +15,43 @@ function checkInputs(){
     const fnameValue = firstName.value.trim()
     const lnameValue = lastName.value.trim()
     const emailValue = email.value.trim()
-    const query = queryType ?  queryType.value.trim(): ''
+    const queryType = document.querySelector('input[name="query_type"]:checked')
     const messageContent = message.value.trim()
+
+    let isValid = true
 
     if(fnameValue === ''){
         setErrorFor(firstName, 'This field is requred')
+        isValid= false
     }
-
     if(lnameValue === ''){
         setErrorFor(lastName, 'This field is requred')
+        isValid= false
     }
-
     if(emailValue === ''){
         setErrorFor(email, 'This field is requred')
-    }else if(!isEmail(emailValue)){
+        isValid= false
+    }
+    if(!isEmail(emailValue)){
         setErrorFor(email,'Please enter a valid email address')
+        isValid= false
     }
-
-    if(query === ''){
+    if(!queryType){
         setErrorFor(queryType, 'Please select a query type')
+        isValid= false
     }
-
     if(messageContent === ''){
         setErrorFor(message, 'This field is requred')
+        isValid= false
     }
-
     if(!checkbox.checked){
         setErrorFor(checkbox, 'To submit this form, please consent to being contacted')
+        isValid= false
     }
 
+    if(isValid){
+        setSuccessFor()
+    }
 }
 
 
@@ -70,4 +78,30 @@ function setErrorFor(input, message){
 
 function isEmail(email){
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+function setSuccessFor(){
+    const queryType = document.querySelector('input[name="query_type"]:checked')
+
+    firstName.value =''
+    lastName.value=''
+    email.value =''
+    queryType.checked= false
+    message.value = ''
+    checkbox.checked = false
+
+    const div = document.createElement('div')
+    div.className = 'success-message'
+
+    div.innerHTML = `
+      <div>
+      <img src="./assets/images/icon-success-check.svg" alt="success-ico">
+      <div class="header-message">Message Sent!</div>
+      </div>
+      <p class="success-text">Thanks for completing the form. We'll be in touch soon!</p>
+    `
+
+    const container = document.querySelector('.contact-container')
+
+    container.appendChild(div)
 }
